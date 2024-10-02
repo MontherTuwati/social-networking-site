c7cd114888d60f735913804906363ced7a20526f
Monther Tuwati <tuwa0001@algonquinlive.com>
2024-10-02 02:43:37 -0400

    Setup Layout for User Authentication

---
+++ app/api/auth/register.ts
@@ -0,0 +1,24 @@
+import dbConnect from '@/lib/dbConnect';
+import User from '@/models/User';
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        await dbConnect();
+
+        const { username, email, password } = await req.json();
+
+        //Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+        }
+
+        //Create a new user
+        const newUser = await User.create({ username, email, password});
+
+        return NextResponse.json(newUser);
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to create user' }, { status: 500 })
+    }
+}
---
+++ models/User.ts
@@ -0,0 +1,24 @@
+import { Document, Schema, model, models } from 'mongoose';
+
+interface IUser extends Document {
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    password: string;
+    profileProfile?: string;
+    createdAt?: Date;
+}
+
+const userSchema = new Schema<IUser>({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    profileProfile: { type: String },
+    createdAt: { type: Date, default: Date.now },
+
+});
+
+const User = models.User || model<IUser>('User', userSchema);
+export default User;
