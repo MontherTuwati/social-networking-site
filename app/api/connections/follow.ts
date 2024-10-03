86eca343279c54fc67d3f8ba7ae477eda3ef24dd
Monther Tuwati <tuwa0001@algonquinlive.com>
2024-10-03 00:09:24 -0400

    implement connection routes for following/unfollowing users

---
+++ app/api/connections/follow.ts
@@ -0,0 +1,26 @@
+import dbConnect from "@/lib/dbConnect";
+import User from "@/models/User";
+import { getSession } from "next-auth/react";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    const session = await getSession({ req });
+    if (!session) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+
+    const { followId } = await req.json();
+    await dbConnect();
+
+    const user = await User.findById(session.user.id);
+    const followUser = await User.findById(followId);
+
+    if (!followUser) return NextResponse.json({ error: 'User to follow not found' }, { status: 404 });
+    if (session.user.id === followId) return NextResponse.json({ error: 'Cannot follow yourself' }, {status: 400});
+
+    if (!user.following.includes(followId)) {
+        user.following.push(followId);
+        await user.save();
+        return NextResponse.json({ message: 'User followd' });
+    }
+
+    return NextResponse.json({ error: 'Already following this user' }, {status: 400});
+}
